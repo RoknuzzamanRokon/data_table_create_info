28db0388da58b76ab82f68d8c78cd3a40a84fec3
rokunuzzamanRokon <rokon.raz@gmail.com>
2024-10-17 20:42:30 +0600

    Add env variable and connect with env.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine, text
-from 
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+db_host = os.getenv('DB_hosty')
+
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
 
 
 table_1 = "vervotech_hotel_map_new"
@@ -10,11 +19,13 @@ table_4 = "vervotech_update_data_info"
 
 
 
-connection_url = f"mysql+pymysql://{db_user}:{db_password}@{db_password}/{db_name}"
+connection_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
 engine = create_engine(connection_url)
 
-with engine.connect() as connection:
-    result = connection.execute(text(f"SHOW TABLES"))
-    for row in result:
-        print(row)
-
+try: 
+    with engine.connect() as connection:
+        result = connection.execute(text(f"SHOW TABLES"))
+        for row in result:
+            print(row)
+except Exception as e:
+    print(f"Error: {e}")
